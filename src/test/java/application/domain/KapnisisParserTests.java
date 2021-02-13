e6cebd03c7dd2a381fd42bd9af576390b96f80c4
kavros <akavroulakis@gmail.com>
2021-02-13 19:05:35 +0000

    extends UT

--- src/test/java/application/domain/KapnisisParserTests.java
+++ src/test/java/application/domain/KapnisisParserTests.java
@@ -48,26 +48,48 @@ public class KapnisisParserTests {
     }
 
     @Test
-    void parse_test(){
+    void parse_onCorrectInput_returnsCorrectOutput(){
         String content = "ΣΧΕΤΙΚΑ ΠΑΡΑΣΤΑΤΙΚΑ\n" +
                 "ΑΓΓΟΥΡΙΑ-ΛΑΣΙΘΙΟΥ-ΠΟΙΟΤΗΤΑ Α 02344403020420 ΚΙΛ 0,00 5,50 0,850 4,68 0,00 4,68 13\n" +
                 "ΚΟΛΟΚΥΘΙΑ-ΧΑΝΙΩΝ-ΠΟΙΟΤΗΤΑ Α 17491850060420 ΚΙΛ 0,00 8,00 1,200 9,60 0,00 9,60 13\n" +
-                "ΤΕΛΑΡΑ IFCO-4320- ΤΕΜ 0,00 1,00 5,000 5,00 0,00 5,00 0\n"+
+                "ΤΕΛΑΡΑ IFCO-4320- ΤΕΜ 0,00 1,00 5,000 5,00 0,00 5,00 0\n" +
+                "ΜΑΙΝΤΑΝΟΣ-ΧΑΝΙΩΝ-ΜΑΤΣΑΚΙ 17491852060420 ΤΕΜ 0,00 10,00 0,280 2,80 0,00 2,80 13\n"+
+                "ΑΝΙΘΟ-ΧΑΝΙΩΝ-ΜΑΤΣΑΚΙ 29871846060420 ΜΑΤ 0,00 5,00 0,280 1,40 0,00 1,40 13\n"+
+                "ΑΓΚΙΝΑΡΕΣ-ΝΑΥΠΛΙΟΥ-ΗΜΕΡΕΣ 328194280320 ΖΕΥ 0,00 3,00 1,200 3,60 0,00 3,60 13\n"+
                 "ΥΠΟΛΟΙΠΑ" ;
 
         List<Product> expected = Arrays.asList(
-                new Product(
-                        "ΑΓΓΟΥΡΙΑ","ΛΑΣΙΘΙΟΥ",
-                        "ΚΙΛ ", "02344403020420",
-                        new BigDecimal("5.50"),new BigDecimal("0.00"),
-                        13,new BigDecimal("0.850"),null
-                ),
+            new Product(
+                    "ΑΓΓΟΥΡΙΑ","ΛΑΣΙΘΙΟΥ",
+                    "ΚΙΛ ", "02344403020420",
+                    new BigDecimal("5.50"),new BigDecimal("0.00"),
+                    13,new BigDecimal("0.850"),null
+            ),
             new Product(
                     "ΚΟΛΟΚΥΘΙΑ","ΧΑΝΙΩΝ",
                     "ΚΙΛ ", "17491850060420",
                     new BigDecimal("8.00"),new BigDecimal("0.00"),
                     13,new BigDecimal("1.200"),null
+            ),
+            new Product(
+                    "ΜΑΙΝΤΑΝΟΣ","ΧΑΝΙΩΝ",
+                    "TEM ", "17491852060420",
+                    new BigDecimal("10.00"),new BigDecimal("0.00"),
+                    13,new BigDecimal("0.280"),null
+            ),
+            new Product(
+                    "ΑΝΙΘΟ","ΧΑΝΙΩΝ",
+                    "ΜΑΤ ", "29871846060420",
+                    new BigDecimal("5.00"),new BigDecimal("0.00"),
+                    13,new BigDecimal("0.280"),null
+            ),
+            new Product(
+                    "ΑΓΚΙΝΑΡΕΣ","ΝΑΥΠΛΙΟΥ",
+                    "ΖΕΥ ", "328194280320",
+                    new BigDecimal("3.00"),new BigDecimal("0.00"),
+                    13,new BigDecimal("1.200"),null
             )
+
         );
 
         List<Product> actual = parser.parse(content);
